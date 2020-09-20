c093d6c3abc0b90296de044f024d19e4ffe4fd25
Charles McVicker <charlie@mcvickernet.com>
2020-09-20 11:25:59 -0400

    Switched to using MutationObservers

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Removes other people's "likes" from your Twitter feed.
 1. Download the file `twitter-fixer.zip` and unzip in your Documents / Files folder.
 2. Open a new tab and go to `about:debugging`
 3. Click `This Firefox` and then `Add Temporary Extension`
-4. When the dialog show up, select the `manifest.json` file
+4. When the dialog shows up, select the `twitter-fixer/manifest.json` file
 5. Tell me if I broke something, please.
--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-function rip_out_likes(){
-  let divs = document.getElementsByClassName('css-1dbjc4n r-obd0qt r-18kxxzh r-zso239');
-  const svg_classes = 'r-111h2gw r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1xzupcd'.split(' ');
-  for (let div of divs) {
-    const parent = div.parentElement;
-    if (parent.childElementCount == 2) {
-      let node = parent.children[1];
-      let count = 6;
-      while (--count) {
-        if (node.childElementCount == 1) node = node.children[0];
-        else break;
-      }
-      if (count == 0) {
-        const text = node.innerText;
-        if (text.endsWith('liked')) div.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style += ';display:None;';
-      }
+function hide_if_like(node) {
+  let div = node.querySelector('.css-1dbjc4n.r-obd0qt.r-18kxxzh.r-zso239');
+  const parent = div.parentElement;
+  if (parent.childElementCount == 2) {
+    let node = parent.children[1];
+    let count = 6;
+    while (--count) {
+      if (node.childElementCount == 1) node = node.children[0];
+      else break;
+    }
+    if (count == 0) {
+      const text = node.innerText;
+      if (text.endsWith('liked')) div.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style += ';display:None;';
     }
   }
 }
 
-document.body.onscroll = rip_out_likes
+let observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    for(let i = 0; i < mutation.addedNodes.length; i++)
+      hide_if_like(mutation.addedNodes[i])
+  });
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
